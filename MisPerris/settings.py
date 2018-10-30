07a41032a68b759bee9d1334b63ddfed10bcf400
cogodyz <c.godoyz@alumnos.duoc.cl>
2018-10-30 04:26:37 -0300

    Nuevamente todo

--- MisPerris/settings.py
+++ MisPerris/settings.py
@@ -125,5 +125,6 @@ STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'), ]
 #Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
