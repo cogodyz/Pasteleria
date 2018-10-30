7e29db11c3e125ffec6bca5cbdb64ed8e348db2e
cogodyz <c.godoyz@alumnos.duoc.cl>
2018-10-30 01:45:49 -0300

    Cambios en settings

--- MisPerris/settings.py
+++ MisPerris/settings.py
@@ -125,6 +125,5 @@ STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'), ]
 #Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
